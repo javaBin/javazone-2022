57ef709d2b179a806a2e6dd50e7695348c0e8b1d
Marek Machnik <40795073+marek-fm@users.noreply.github.com>
2022-08-11 22:01:25 +0200

    Rettet tekst plassering

--- src/pages/Workshop/WorkshopPage.tsx
+++ src/pages/Workshop/WorkshopPage.tsx
@@ -6,8 +6,7 @@ import {Workshop} from "./Workshop";
 export function WorkshopPage(){
     return (
         <>
-            <VikingBanner header="JavaZone Workshops 2022" subHeader="Find your workshop" />
-            <p>Registration opens August 22nd at 12:00.</p>
+            <VikingBanner header="JavaZone Workshops 2022" subHeader="Find your workshop - Registration opens on August 22nd at 12:00." />
             <CenterSection color="blue">
                 <Workshop/>
             </CenterSection>
