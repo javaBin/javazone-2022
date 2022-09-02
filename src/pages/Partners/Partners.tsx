7a77053f964b6217885f2a9dce8dfd6fba856932
Alexander Svendsen <alexandersvendsen1990@gmail.com>
2022-09-02 09:07:51 +0200

    fix: linting

--- src/pages/Partners/Partners.tsx
+++ src/pages/Partners/Partners.tsx
@@ -17,7 +17,7 @@ function shuffle(array: Partner[]): Partner[] {
     let currentIndex = array.length,  randomIndex;
 
     // While there remain elements to shuffle.
-    while (currentIndex != 0) {
+    while (currentIndex !== 0) {
 
         // Pick a remaining element.
         randomIndex = Math.floor(Math.random() * currentIndex);
